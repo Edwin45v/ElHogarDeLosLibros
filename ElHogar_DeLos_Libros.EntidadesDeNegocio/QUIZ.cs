64ea98b90ce360594a0edd42bed26cb93417f0e8
Brenda <brendaleiva161@gmail.com>
2023-09-11 11:50:30 -0600

    Programacion de la tabla quiz de EN

--- ElHogar_DeLos_Libros.EntidadesDeNegocio/QUIZ.cs
+++ ElHogar_DeLos_Libros.EntidadesDeNegocio/QUIZ.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,22 @@ namespace ElHogar_DeLos_Libros.EntidadesDeNegocio
 {
     public class QUIZ
     {
+        [Key]
+        public int id { get; set; }
 
+        [Required(ErrorMessage = "Tu pregunta es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Pregunta { get; set; }
+
+
+        [Required(ErrorMessage = "Tu respuesta  es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Respuesta { get; set; }
+
+
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+        public List<Libros> Libros { get; set; }
     }
 }
