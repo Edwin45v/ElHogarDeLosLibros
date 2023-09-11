0084695afe2c86f5b49d14dcdeca7ff1b9796828
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-11 09:57:36 -0600

    Tablas de Entidades de Negocio

---
+++ ElHogar_DeLos_Libros.EntidadesDeNegocio/Rol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElHogar_DeLos_Libros.EntidadesDeNegocio
+{
+    public class Rol
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+    }
+}
