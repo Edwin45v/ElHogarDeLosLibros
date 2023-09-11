ced10366df3aa4956f0be7a61cbbbbd374187a9d
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-11 11:28:15 -0600

    Programacion de capa Libros

--- ElHogar_DeLos_Libros.EntidadesDeNegocio/Libros.cs
+++ ElHogar_DeLos_Libros.EntidadesDeNegocio/Libros.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace ElHogar_DeLos_Libros.EntidadesDeNegocio
 {
     public class Libros
     {
+        public int id { get; set; }
+        public int IdQuiz { get; set; }
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Categoria { get; set; }
+        public string Imagen { get; set; }
     }
 }
