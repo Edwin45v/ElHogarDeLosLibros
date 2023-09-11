631634db6c2b590caf16851c4e98f95d118e3888
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-11 10:36:52 -0600

    Programacion de la tabla de Rol y Usuario

--- ElHogar_DeLos_Libros.EntidadesDeNegocio/Usuario.cs
+++ ElHogar_DeLos_Libros.EntidadesDeNegocio/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,51 @@ namespace ElHogar_DeLos_Libros.EntidadesDeNegocio
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int IdRol { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Apellido es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Login es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+
+        [Display(Name = "Fecha Registro")]
+        public DateTime FechaRegistro { get; set; }
+
+        public Rol Rol { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Confirmar el password")]
+        [StringLength(32, ErrorMessage = "Pasword debe estar entre 5 a 32 caracteres", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Pasword", ErrorMessage = "Password y confirmar password deben de ser iguales")]
+        [Display(Name = "Confirmar password")]
+        public string confirmPassword_aux { get; set; }
+    }
+    public enum Estatus_Usuario
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
     }
 }
