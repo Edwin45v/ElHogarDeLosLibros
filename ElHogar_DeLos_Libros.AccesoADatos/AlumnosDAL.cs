6fb8d59349a5214a2efa5a7a9592c5ccedec8afc
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-13 11:38:31 -0600

    lines de namespace

--- ElHogar_DeLos_Libros.AccesoADatos/AlumnosDAL.cs
+++ ElHogar_DeLos_Libros.AccesoADatos/AlumnosDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ElHogar_DeLos_Libros.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ ElHogar_DeLos_Libros.PruebasUnitarias/RolDALTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElHogar_DeLos_Libros.AccesoADatos;
+using ElHogar_DeLos_Libros.EntidadesDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElHogar_DeLos_Libros.AccesoADatos.Tests
+{
+    [TestClass()]
+    public class RolDALTests
+    {
+        private static Rol rolInicial = new Rol { Id = 2 };
+        [TestMethod()]
+        public void CrearAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CrearAsyncTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerPorIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerTodosAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
