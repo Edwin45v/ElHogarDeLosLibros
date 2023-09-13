aad9f117948c4f8522074244d829965ddbc92bbb
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-13 09:52:12 -0600

    Configuracion roldal

--- ElHogar_DeLos_Libros.AccesoADatos/RolDAL.cs
+++ ElHogar_DeLos_Libros.AccesoADatos/RolDAL.cs
@@ -1,99 +1,97 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ElHogar_DeLos_Libros.EntidadesDeNegocio;
 
-namespace ElHogar_DeLos_Libros.AccesoADatos;
-
-public class RolDAL
+namespace ElHogar_DeLos_Libros.AccesoADatos
 {
-
-    public static async Task<int> CrearAsync(Rol pRol)
+    public class RolDAL
     {
-        int result = 0;
-        using (var bdContexto = new BDContexto())
+        public static async Task<int> CrearAsync(Rol pRol)
         {
-            bdContexto.Add(pRol);
-            result = await bdContexto.SaveChangesAsync();
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                bdContexto.Add(pRol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
         }
-        return result;
-    }
 
-    public static async Task<int> ModificarAsync(Rol pRol)
-    {
-        int result = 0;
-        using (var bdContexto = new BDContexto())
+        public static async Task<int> ModificarAsync(Rol pRol)
         {
-            var rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
-            rol.Nombre = pRol.Nombre;
-            bdContexto.Update(rol);
-            result = await bdContexto.SaveChangesAsync();
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+                rol.Nombre = pRol.Nombre;
+                bdContexto.Update(rol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
         }
-        return result;
-    }
 
-    public static async Task<int> EliminarAsync(Rol pRol)
-    {
-        int result = 0;
-        using (var bdContexto = new BDContexto())
+        public static async Task<int> EliminarAsync(Rol pRol)
         {
-            var rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
-            bdContexto.Rol.Remove(rol);
-            result = await bdContexto.SaveChangesAsync();
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+                bdContexto.Rol.Remove(rol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
         }
-        return result;
-    }
 
-    public static async Task<Rol> ObtenerPorIdAsync(Rol pRol)
-    {
-        var rol = new Rol();
-        using (var bdContexto = new BDContexto())
+        public static async Task<Rol> ObtenerPorIdAsync(Rol pRol)
         {
-            rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+            var rol = new Rol();
+            using (var bdContexto = new BDContexto())
+            {
+                rol = await bdContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+            }
+            return rol;
         }
-        return rol;
-    }
 
-    public static async Task<List<Rol>> ObtenerTodosAsync()
-    {
-        var roles = new List<Rol>();
-        using (var bdContexto = new BDContexto())
+        public static async Task<List<Rol>> ObtenerTodosAsync()
         {
-            roles = await bdContexto.Rol.ToListAsync();
+            var roles = new List<Rol>();
+            using (var bdContexto = new BDContexto())
+            {
+                roles = await bdContexto.Rol.ToListAsync();
+            }
+            return roles;
         }
-        return roles;
-    }
 
-    internal static IQueryable<Rol> QuerySelect(IQueryable<Rol> pQuery, Rol pRol)
-    {
-        if (pRol.Id > 0)
-            pQuery = pQuery.Where(s => s.Id == pRol.Id);
-
-        if (!string.IsNullOrWhiteSpace(pRol.Nombre))
-            pQuery = pQuery.Where(s => s.Nombre.Contains(pRol.Nombre));
-
-        pQuery = pQuery.OrderByDescending(s => s.Id).AsQueryable();
-
-        if (pRol.Top_Aux > 0)
-            pQuery = pQuery.Take(pRol.Top_Aux).AsQueryable();
-
-        return pQuery;
-    }
-
-    public static async Task<List<Rol>> BuscarAsync(Rol pRol)
-    {
-        var roles = new List<Rol>();
-        using (var bdContexto = new BDContexto())
+        internal static IQueryable<Rol> QuerySelect(IQueryable<Rol> pQuery, Rol pRol)
         {
-            var select = bdContexto.Rol.AsQueryable();
-            select = QuerySelect(select, pRol);
-            roles = await select.ToListAsync();
-        }
-        return roles;
-    }
+            if (pRol.Id > 0)
+                pQuery = pQuery.Where(s => s.Id == pRol.Id);
 
+            if (!string.IsNullOrWhiteSpace(pRol.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pRol.Nombre));
+
+            pQuery = pQuery.OrderByDescending(s => s.Id).AsQueryable();
+
+            if (pRol.Top_Aux > 0)
+                pQuery = pQuery.Take(pRol.Top_Aux).AsQueryable();
+
+            return pQuery;
+        }
+
+        public static async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            var roles = new List<Rol>();
+            using (var bdContexto = new BDContexto())
+            {
+                var select = bdContexto.Rol.AsQueryable();
+                select = QuerySelect(select, pRol);
+                roles = await select.ToListAsync();
+            }
+            return roles;
+        }
+    }
 }
-
