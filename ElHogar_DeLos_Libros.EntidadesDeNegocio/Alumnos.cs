73d90f381bdb99a99966c60e6d019c512e10f699
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2023-09-13 08:27:17 -0600

    Codificacion de la tabla Alumnos de la Capa EN

--- ElHogar_DeLos_Libros.EntidadesDeNegocio/Alumnos.cs
+++ ElHogar_DeLos_Libros.EntidadesDeNegocio/Alumnos.cs
@@ -8,5 +8,28 @@ namespace ElHogar_DeLos_Libros.EntidadesDeNegocio
 {
     public class Alumnos
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Grado")]
+        [Required(ErrorMessage = "El grado es requerido")]
+        [Display(Name = "Grado")]
+        public int GradoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "El largo maximo es 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [MaxLength(100, ErrorMessage = "El largo maximo es 100 caracteres")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "La imagen es requerido")]
+        [MaxLength(200, ErrorMessage = "El largo maximo es 200 caracteres")]
+        public string Imagen { get; set; }
+        [NotMapped]
+        public int top_aux { get; set; }
+        public Alumnos_Grado { get; set; }
     }
 }
+
