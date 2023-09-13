ef2bdf997fc9a243f4f16d1d036df7267a24cf79
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-09-13 08:30:41 -0600

    Agrege las clases de Acceso a datos

---
+++ ElHogar_DeLos_Libros.AccesoADatos/LibrosDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElHogar_DeLos_Libros.AccesoADatos
+{
+    internal class LibrosDAL
+    {
+    }
+}
